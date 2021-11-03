4074aa517631cff1dcdca5706d09bb12f840e4c6
Knud Ronau <Knudronau@hotmail.com>
2021-11-03 20:40:08 +0100

    MainFrame progress

--- src/main/java/com/dt181g/project/MainFrame.java
+++ src/main/java/com/dt181g/project/MainFrame.java
@@ -1,10 +1,23 @@
 package com.dt181g.project;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 600;
+    private JPanel contentpane;
 
     public MainFrame() {
+        contentpane = new JPanel();
+        contentpane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentpane);
+        contentpane.setLayout(BorderLayout);
+        pack();
+    }
 
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 }
